4ab0db468c98631a8d523ad2cecefdec48988146
kyungwon <kyungwon0928@daum.net>
2023-11-13 23:14:01 +0900

    design: 전체 color 수정 - 동일한 위젯, 배경색상 통일 - constants.dart에 색상 변수 추가

--- dressmeup/lib/assets/constants.dart
+++ dressmeup/lib/assets/constants.dart
@@ -1,1 +1,6 @@
 const int homepageBackgroundColor = 0xff111111;
+const int buttonColor = 0xFFA9DDF7;
+const int completeButtonColor = 0xFFF69A9A;
+const int widgetBackgroundColor = 0xFFFFEAEA;
+const int boxBorderColor = 0xFF616161;
+const int fontColor = 0xFF616161;
