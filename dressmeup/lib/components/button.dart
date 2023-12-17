f53ca776e97b0f350da5d79fedd40b756ad5676f
kyungwon <kyungwon0928@daum.net>
2023-12-17 17:20:54 +0900

    feat: camera, gallery로부터 이미지 불러오기(ing)

--- dressmeup/lib/components/button.dart
+++ dressmeup/lib/components/button.dart
@@ -1,8 +1,10 @@
+import 'dart:io';
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:flutter/material.dart';
 import 'package:dressmeup/assets/constants.dart';
 import 'package:image_picker/image_picker.dart';
 
-class Button extends StatelessWidget {
+class Button extends StatefulWidget {
   final String buttonName;
   final IconData icon;
   final ImageSource imgSource;
@@ -13,14 +15,20 @@ class Button extends StatelessWidget {
       required this.icon,
       required this.imgSource});
 
+  @override
+  State<Button> createState() => _ButtonState();
+}
+
+class _ButtonState extends State<Button> {
   Future getImage(ImageSource imageSource) async {
     final ImagePicker picker = ImagePicker();
     final XFile? pickedFile = await picker.pickImage(source: imageSource);
 
     if (pickedFile != null) {
-      // setState(() {
-      //   _image = XFile(pickedFile.path)
-      // }
+      setState(() {
+        DefaultCacheManager().getSingleFile(pickedFile.path, key: 'file1');
+      });
+      print(pickedFile.path);
     }
   }
 
@@ -28,12 +36,12 @@ class Button extends StatelessWidget {
   Widget build(BuildContext context) {
     return TextButton(
       onPressed: () {
-        getImage(imgSource);
+        getImage(widget.imgSource);
       },
       style: TextButton.styleFrom(
         backgroundColor: Colors.white,
         side: const BorderSide(color: Color(boxBorderColor)),
-        shape: buttonName == "closet"
+        shape: widget.buttonName == "closet"
             ? RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(12),
                 side: const BorderSide(
@@ -48,18 +56,18 @@ class Button extends StatelessWidget {
               ),
       ),
       child: Center(
-        child: buttonName == "closet"
+        child: widget.buttonName == "closet"
             ? const Icon(Icons.add, color: Color(boxBorderColor), size: 50)
             : Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Text(
-                    buttonName,
+                    widget.buttonName,
                     style: const TextStyle(
                         fontSize: 18, color: Color(boxBorderColor)),
                   ),
                   const SizedBox(height: 10),
-                  Icon(icon, size: 40, color: const Color(buttonColor))
+                  Icon(widget.icon, size: 40, color: const Color(buttonColor))
                 ],
               ),
       ),
